61fb5cced7a2cdf423401f7daf5cc9775676b5ef
itwangdong001 <itwangdong001@sina.com>
2016-08-17 23:45:08 +0800

    this is my personal webset.

---
+++ itwangdong001@sina.com/src/use.js
@@ -0,0 +1,16 @@
+addEvent(window, 'load', function (){
+	sixCirl();
+	addEvent(window, 'scroll',function(){
+		sixCirl();});
+	dragLy();
+	dragBox();
+	fnSkey();		
+	myGame();
+	fnSlide();
+	textShodow();
+	fnshake();
+	fnMynav();
+	fnMySfq();
+	myPicLoad();
+	fnMyLast();
+});
